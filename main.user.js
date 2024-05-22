b9a27815a00071bccb8a372ea2bfb80642787dee
CycleBai <unbounded.sydney@gmail.com>
2024-05-22 22:40:50 +1000

    [ADD] 新增 AGPL 许可证。

--- main.user.js
+++ main.user.js
@@ -12,6 +12,23 @@
 // @description 奥德思阔 oSafety，为您的安全保驾护航。
 // ==/UserScript==
 
+/*
+    Copyright (C) 2024  Oldsquaw
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 
 (function() {
     'use strict';
